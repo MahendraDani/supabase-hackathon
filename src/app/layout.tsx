aa3546f9005f185570af5925dab22819b9d83660
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-10 16:18:03 +0530

    feat: configured theme toggle using shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import "@/app/globals.css";
 import { Inter as FontSans } from "next/font/google"
+import { ThemeProvider } from "@/components/custom/theme/theme-provider";
 
 import { cn } from "@/lib/utils";
 
@@ -22,13 +23,19 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
+
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable
+      )}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
