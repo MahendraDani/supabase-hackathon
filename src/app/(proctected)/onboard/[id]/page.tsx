dcf0b8f298232f3962cad7fd0ccaf51a7d075ac1
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-11 19:12:04 +0530

    feat: created onboarding page with zod validation and insertion in profiles table

--- src/app/(proctected)/onboard/[id]/page.tsx
+++ src/app/(proctected)/onboard/[id]/page.tsx
@@ -19,61 +19,108 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { ToastAction } from "@/components/ui/toast"
+import { useToast } from "@/components/ui/use-toast"
+import { z } from "zod";
 
 interface PageProps {
   params: {
     id: string;
   }
 }
+const onboardingFormSchema = z.object({
+  fullName: z.string().min(3, { message: "Name must be atleast 3 characters long" }),
+  username: z.string().min(6, { message: "Username must be atleast 6 characters long" })
+})
+
 export default function ({ params }: PageProps) {
-  const [name, setName] = useState("");
+  const { toast } = useToast()
+  const [fullName, setfullName] = useState("");
   const [username, setUsername] = useState("");
+  const [gender, setGender] = useState("");
+  const [isFullNameError, setIsFullNameError] = useState(false);
+  const [fullNameErrorMessage, setFullNameErorrMessage] = useState("");
+  const [isUsernameError, setIsUsernameError] = useState(false);
+  const [usernameErrorMessage, SetUserNameErrorMessage] = useState("");
+
+
 
   const handleFormSubmit = async () => {
-    // TODO : insert user name and user id of this user in profiles table
+
+    if (!fullName || !username || !gender) {
+      toast({
+        title: "Incorrect details",
+        description: "All fields are required",
+        action: <ToastAction altText="Try again">Try again</ToastAction>,
+      })
+      return;
+    }
+
+
+    const parsedInput = onboardingFormSchema.safeParse({ fullName, username });
+    if (!parsedInput.success) {
+      const parsedErrors = parsedInput.error.issues;
+      parsedErrors.forEach((e) => {
+        console.log(e.path[0]);
+        if (e.path[0] === "fullName") {
+          setIsFullNameError(true);
+          setFullNameErorrMessage(e.message)
+        } else if (e.path[0] === "username") {
+          setIsUsernameError(true);
+          SetUserNameErrorMessage(e.message)
+        }
+      })
+      return;
+    }
+
+
     const supabase = createClientComponentClient();
     const { data, error } = await supabase.from("profiles").insert([{
-      user_id: params.id, full_name: name, username: username
+      user_id: params.id, full_name: fullName, username: username
     }]);
 
     if (error) {
-      alert(JSON.stringify(error, null, 2));
+      // TODO : handle error when username is not unique
+      if (error.code === "23505") {
+        toast(
+          {
+            title: "username already taken",
+            description: "The username is already taken by someone please try any other username",
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+          }
+        )
+      }
 
-      // Error : when input username is not unique 
-      // CODE : 23505
-      // message : duplicate key values violate unique constraint 
       return;
     }
-    console.log(data);
+    alert("Yay, profile created")
+    // TODO : redirect user to home page instead
   }
   return (
     <>
       <div className="w-full min-h-[40rem] flex justify-center items-center">
-        {/* <form onSubmit={handleFormSubmit} className="flex flex-col gap-2">
-          <Input id="full name" placeholder="Please write your name" onChange={e => setName(e.target.value)} />
-          <Input id="user name" placeholder="Please write your user name" onChange={e => setUsername(e.target.value)} />
-          <Button type="submit">Submit</Button>
-        </form> */}
-
         <Card >
           <CardHeader className="text-center">
             <CardTitle>Create Profile</CardTitle>
             <CardDescription>Others will be able to see the following profile details</CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
+            <form >
               <div className="grid w-full items-center gap-4">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Full Name</Label>
-                  <Input id="name" placeholder="Jhon Doe" />
+                  <Input id="name" placeholder="Jhon Doe" onChange={e => setfullName(e.target.value)} />
+                  {isFullNameError && <span className="text-red-400 text-sm">{fullNameErrorMessage}</span>}
+
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="username">Username</Label>
-                  <Input id="username" placeholder="jhondoe@13" />
+                  <Input id="username" placeholder="jhondoe@13" onChange={e => setUsername(e.target.value)} />
+                  {isUsernameError && <span className="text-red-400 text-sm">{usernameErrorMessage}</span>}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="gender">Gender</Label>
-                  <Select>
+                  <Select onValueChange={(value) => { setGender(value) }}>
                     <SelectTrigger id="framework">
                       <SelectValue placeholder="Male" />
                     </SelectTrigger>
@@ -84,16 +131,19 @@ export default function ({ params }: PageProps) {
                     </SelectContent>
                   </Select>
                 </div>
+
               </div>
             </form>
+
           </CardContent>
           <CardFooter className="flex justify-center items-center">
-            <Button>Create</Button>
+            <Button onClick={handleFormSubmit}>Create</Button>
           </CardFooter>
         </Card>
       </div>
     </>
   )
 }
+
 
 // Aim of this test is to insert the user name of the logged in user and it's user id
