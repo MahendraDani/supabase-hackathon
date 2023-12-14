8ce3bdcf39a938e2556da9db7b37cda95118c944
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-14 10:24:21 +0530

    feat: created boiler plate for all user pages

---
+++ src/app/(proctected)/profile/[username]/favourties/page.tsx
@@ -0,0 +1,7 @@
+export default function FavouritesPage() {
+  return (
+    <div>
+      ALl your likes posts will be shown here in favourites sections
+    </div>
+  )
+}
---
+++ src/app/(proctected)/profile/[username]/reads/page.tsx
@@ -0,0 +1,5 @@
+export default function ReadsPage() {
+  return (
+    <div>ALl read post by a user will be shown here</div>
+  )
+}
