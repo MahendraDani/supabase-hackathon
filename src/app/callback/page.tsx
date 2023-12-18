75d6058e6f20533a10244c721678df71d48d1a91
Mahendra Dani <danimahendra0904@gmail.com>
2023-12-18 13:16:05 +0530

    fix: fixing vercel errpr

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,4 +1,4 @@
-export default function CallBack() {
+const CallBack = () => {
   return (
     <div className="min-h-[30rem] w-full flex justify-center items-center">
       <h1 className="md:text-4xl text-2xl text-black font-bold dark:text-sky-200">
@@ -7,3 +7,4 @@ export default function CallBack() {
     </div>
   )
 }
+export default CallBack
